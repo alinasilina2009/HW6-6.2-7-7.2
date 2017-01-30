4d3d5ad2bc8a3894c7a4d4adcd8ae6ca0536dce2
Alina Stepovik <alina-stepovik@ukr.net>
2017-01-30 23:35:55 +0200

    Переделки HW6,HW6.5,HW7, новая HW7.2

---
+++ js/init.js
@@ -0,0 +1,12 @@
+;(function($){
+	//init bpopap
+ 	$('._js-btn-open-modal').on('click', function (){
+ 	 	$('#modal-order').bPopup({
+ 	 	closeClass:'modal-close',
+ 	 	});
+ 	});
+
+	//init formstyler
+	$('select').styler();
+})(jQuery);
+
